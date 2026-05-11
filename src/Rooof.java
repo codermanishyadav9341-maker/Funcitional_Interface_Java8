3ec7a6019cbc0953fb347b5979b60db71c26eb25
codermanishyadav9341@gmail.com <you@example.com>
2026-05-11 17:32:43 +0530

    final

--- src/Rooof.java
+++ src/Rooof.java
@@ -1,50 +1,65 @@
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Scanner;
+//import java.util.stream.*;
+//
+//class Employee12 {
+//    int id;
+//    String name;
+//
+//    Employee12(int id, String name) {
+//        this.id = id;
+//        this.name = name;
+//    }
+//}
+//
+// class Q26 {
+//    public static void main(String[] args) {
+//
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.print("Enter number of employees: ");
+//        int n = sc.nextInt();
+//
+//        List<Employee12> list = new ArrayList<>();
+//
+//        for (int i = 0; i < n; i++) {
+//            System.out.print("Enter id: ");
+//            int id = sc.nextInt();
+//
+//            System.out.print("Enter name: ");
+//            String name = sc.next();
+//
+//            list.add(new Employee12(id, name));
+//        }
+//
+//        List<Employee12> res = list.stream()
+//                .collect(Collectors.collectingAndThen(
+//                        Collectors.toMap(
+//                                e -> e.id,
+//                                e -> e,
+//                                (a, b) -> a   // duplicate id me first ko rakhega
+//                        ),
+//                        m -> new ArrayList<>(m.values())
+//                ));
+//
+//        System.out.println("\nAfter removing duplicate IDs:");
+//        res.forEach(e -> System.out.println(e.id + " " + e.name));
+//    }
+//}
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.*;
 
-class Employee12 {
-    int id;
-    String name;
+import java.util.Optional;
 
-    Employee12(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-}
+class Roof{
+    public static void main(String[] args){
 
- class Q26 {
-    public static void main(String[] args) {
+//        String name = null;
+//        System.out.println(name.length());
 
-        Scanner sc = new Scanner(System.in);
+        Optional<String> name = Optional.ofNullable(null);
+        System.out.println(name.orElse(null).length());
 
-        System.out.print("Enter number of employees: ");
-        int n = sc.nextInt();
-
-        List<Employee12> list = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            System.out.print("Enter id: ");
-            int id = sc.nextInt();
-
-            System.out.print("Enter name: ");
-            String name = sc.next();
-
-            list.add(new Employee12(id, name));
-        }
-
-        List<Employee12> res = list.stream()
-                .collect(Collectors.collectingAndThen(
-                        Collectors.toMap(
-                                e -> e.id,
-                                e -> e,
-                                (a, b) -> a   // duplicate id me first ko rakhega
-                        ),
-                        m -> new ArrayList<>(m.values())
-                ));
-
-        System.out.println("\nAfter removing duplicate IDs:");
-        res.forEach(e -> System.out.println(e.id + " " + e.name));
     }
 }
