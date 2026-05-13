2c3d09ab5c7908775d017779620a6c7c6549e4e4
codermanishyadav9341@gmail.com <you@example.com>
2026-05-13 13:01:23 +0530

    anyMatch() StreamAPI

---
+++ src/AnyMatch_StreamAPI/GreaterNumber.java
@@ -0,0 +1,27 @@
+package AnyMatch_StreamAPI;
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+public class GreaterNumber {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter Size:- ");
+        int size = scan.nextInt();
+
+        List<Integer> numbers = new ArrayList<>();
+
+        System.out.println("Enter your Array elements:- ");
+        for(int i = 0; i<size; i++){
+            numbers.add(scan.nextInt());
+        }
+
+        boolean result =
+                numbers.stream()
+                        .anyMatch(num-> num > 50);
+        System.out.println(result);
+
+        scan.close();
+    }
+}
