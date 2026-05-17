14336dec674cf32e3ed5adc60842ff0d702b902e
codermanishyadav9341@gmail.com <you@example.com>
2026-05-17 17:45:22 +0530

    Basic StreamAPI Pratice

---
+++ src/Basic_StreamAPI/AscendingOrder.java
@@ -0,0 +1,11 @@
+package Basic_StreamAPI;
+
+import java.util.Scanner;
+
+public class AscendingOrder {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter Size:- ");
+    }
+}
---
+++ src/Basic_StreamAPI/FirstElement.java
@@ -0,0 +1,29 @@
+package Basic_StreamAPI;
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Optional;
+public class FirstElement {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter Size:- ");
+        int size = scan.nextInt();
+
+        List<Integer> numbers = new ArrayList<>();
+
+        System.out.println("Enter your Array elements:- ");
+        for(int i = 0; i<size; i++){
+            numbers.add(scan.nextInt());
+        }
+
+        Optional<Integer> result =
+                numbers.stream()
+                        .findFirst();
+
+        System.out.println("Array first Digits:- " +result.get());
+
+        scan.close();
+    }
+}
