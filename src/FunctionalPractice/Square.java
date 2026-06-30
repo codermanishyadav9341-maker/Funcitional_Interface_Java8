233011cba3003e43cee0340876bbea180eb5982e
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-06-30 16:45:57 +0530

    Update

--- src/FunctionalPractice/Square.java
+++ src/FunctionalPractice/Square.java
@@ -7,10 +7,18 @@ public interface Square {
 }
 
 class Result1 implements Square{
-    Scanner scan = new Scanner(System.in);
-
     @Override
     public void square(int num){
+       System.out.println("Square number is:- " +(num*num));
+    }
 
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter your number:- ");
+        int num = scan.nextInt();
+
+        Result1 rr = new Result1();
+        rr.square(num);
     }
 }
