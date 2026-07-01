0da465629faf4fc8c985c93e2544602676eab2f5
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-07-01 10:43:50 +0530

    Update

---
+++ src/FunctionalPractice/AbsoluteNumber.java
@@ -0,0 +1,27 @@
+package FunctionalPractice;
+
+import java.util.Scanner;
+@FunctionalInterface
+public interface AbsoluteNumber {
+    void printAbsolute(int num);
+}
+
+class AbsoluteImpl  implements AbsoluteNumber{
+    @Override
+    public void printAbsolute(int num){
+        int result = Math.abs(num);
+        System.out.println("Absolut number is:- " +result);
+    }
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter your number is:- ");
+        int num = scan.nextInt();
+
+        AbsoluteNumber  absolute = new AbsoluteImpl();
+        absolute.printAbsolute(num);
+
+        scan.close();
+    }
+}
