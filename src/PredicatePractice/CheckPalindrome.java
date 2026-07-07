7b901615bcaaae39b95b08b316620f22140617d9
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-07-07 12:20:18 +0530

    Initial commit

---
+++ src/PredicatePractice/CheckPalindrome.java
@@ -0,0 +1,20 @@
+package PredicatePractice;
+
+import java.util.Scanner;
+import java.util.function.Predicate;
+public class CheckPalindrome {
+    public static void main(String[] args){
+        try(Scanner scan = new Scanner(System.in)){
+
+            Predicate<String> isPalindrome = (str) -> {
+                String reverse = new StringBuilder(str).reverse().toString();
+                return reverse.equals(str);
+            };
+
+            System.out.print("Enter character:- ");
+            String str = scan.nextLine();
+
+            System.out.println("Chek palindrome:- " +isPalindrome.test(str));
+        }
+    }
+}
