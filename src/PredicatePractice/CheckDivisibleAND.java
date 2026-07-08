d5dcc77a0aa61e7d5d3dd6e16f5e978e2791097c
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-07-08 09:31:04 +0530

    Initial commit

--- src/PredicatePractice/CheckDivisibleAND.java
+++ src/PredicatePractice/CheckDivisibleAND.java
@@ -3,17 +3,16 @@ package PredicatePractice;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.function.Predicate;
-public class CheckDivisible1 {
+public class CheckDivisibleAND {
     public static void main(String[] args){
         try(Scanner scan = new Scanner(System.in)){
 
-            Predicate<Integer> isDivisible = (num) -> (num%2 == 0 && num%5 == 0);
+            Predicate<Integer> isDivisible = (num) -> (num%4 == 0&& num%6 == 0);
 
             System.out.print("Enter number:- ");
             int num = scan.nextInt();
 
-            System.out.println("Check divisible by 2 && 5:- " +isDivisible.test(num));
-
+            System.out.println("Check divisible by 4 && 6:- " +isDivisible.test(num));
         }
         catch(InputMismatchException e){
             System.out.println("Invalid input");
