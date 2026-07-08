06e6ca528de6a37c242cf3d93f3bc321eb824a41
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-07-08 09:44:05 +0530

    Initial commit

--- src/PredicatePractice/NumberCheck.java
+++ src/PredicatePractice/NumberCheck.java
@@ -3,16 +3,16 @@ package PredicatePractice;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.function.Predicate;
-public class CheckDivisibleBy {
+public class NumberCheck {
     public static void main(String[] args){
         try(Scanner scan = new Scanner(System.in)){
 
-            Predicate<Integer> isNotDivisible3 = (num) -> num%3 != 0;
+            Predicate<Integer> isCheck = (num) -> num >= 0 && num < 100;
 
             System.out.print("Enter number:- ");
             int num = scan.nextInt();
 
-            System.out.println("Not divisible by 3:- " + isNotDivisible3.test(num));
+            System.out.println("Number greaterThan >= 0 && lessThan < 100:- " +isCheck.test(num));
         }
         catch(InputMismatchException e){
             System.out.println("Invalid input");
