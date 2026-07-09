93fc9f1ba7c74d7e61aa841c071e533a22813bc2
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-07-09 11:01:06 +0530

    Initial commit

---
+++ src/FunctionPracticeSet/DoubleNumber.java
@@ -0,0 +1,21 @@
+package FunctionPracticeSet;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.function.Function;
+public class DoubleNumber {
+    public static void main(String[] args){
+        try(Scanner scan = new Scanner(System.in)){
+
+            Function<Integer,Integer> doubleNumber = (num) -> num*2;
+
+            System.out.print("Enter number:- ");
+            int num = scan.nextInt();
+
+            System.out.println("Double number:- " +doubleNumber.apply(num));
+        }
+        catch(InputMismatchException e){
+            System.out.println("Please enter a valid integer");
+        }
+    }
+}
